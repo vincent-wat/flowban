30dc759ad38dc9bddaf5046f221621bb5d9084b9
Emilio <emiliomontes10@gmail.com>
2024-10-17 21:32:22 -0700

    created Routers for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
-import LoginPage from './Components/Login/LoginPage.jsx';
-import Profile from './Components/Login/Profile/EditUser.js';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './Components/Profile/EditUser'; 
+import LoginPage from './Components/Login/LoginPage'; 
+//import Home from './Home'; 
 
 function App() {
   return (
-    <div>
-      <LoginPage/>
-      <Profile/>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Flowban</h1>
+        
+        <Routes>
+          {/* Define routes for different pages */}
+          <Route path="/" /> {/* create home page later */}
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
