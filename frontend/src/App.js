af0ece3d2433269e296aaf096301f9544299bd60
Emilio <emiliomontes10@gmail.com>
2024-10-22 12:15:52 -0700

    improve profile section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './Components/Profile/EditUser'; 
 import LoginPage from './Components/Login/LoginPage'; 
-//import Home from './Home'; 
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>Flowban</h1>
-        
         <Routes>
           {/* Define routes for different pages */}
           <Route path="/" /> {/* create home page later */}
           <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
-      </div>
     </Router>
   );
 }
