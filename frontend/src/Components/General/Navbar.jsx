fd93f9c91291b9a059b6e2e322c676655d21d41f
Erick <erick.espinoza.710@my.csun.edu>
2024-10-23 18:29:30 -0700

    Added a basic nav bar

---
+++ frontend/src/Components/General/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath} from 'react-router-dom';
+import '../General/Navbar.css';
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-name"> FlowBan </Link>
+            <ul>
+                <CustomLinks to="/login"> Login </CustomLinks>
+                <CustomLinks to='/profile'> Profile </CustomLinks>
+            </ul>
+        </nav>
+    );
+}
+
+// custom link prop for routes in the navbar
+// the useResolvedPath allows for relative or absolute paths to enures full paths no
+// matter what
+
+function CustomLinks({to, children, ...props}) {
+    //const path = window.location.pathname
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    return (
+        <li className={isActive? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
+
+// 
