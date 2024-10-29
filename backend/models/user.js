08e77a9e24e67642921ee60f78a0ac366e9d5280
Emilio <emiliomontes10@gmail.com>
2024-10-29 11:14:40 -0700

    created user attributes

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,24 +2,47 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('./database');  // Import the sequelize instance from database.js
 
 const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+    validate: {
+      isEmail: true,
+    },
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      is: /^[0-9\-+()]*$/,
+    },
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_roles: {
+    type: DataTypes.ENUM('Admin', 'User', 'Moderator'),
+    allowNull: false,
+    defaultValue: 'User',
+  },
 }, {
   timestamps: true,
   createdAt: 'created_at',
-  updatedAt: false,
+  updatedAt: 'updated_at',
 });
 
 module.exports = User;
