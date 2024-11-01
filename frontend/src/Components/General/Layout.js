3ec02f36bbb3c18ab26809e8146d8ac66b8105e8
Vincent Paraiso <vincep0330@gmail.com>
2024-10-31 18:19:27 -0700

    Fixed Navbar

--- frontend/src/Components/General/Layout.js
+++ frontend/src/Components/General/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Layout({ children }) {
   return (
     <div style={{ width: '100vw', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <main style={{ flex: '1', padding: '80px 0 0 0' }}> {/* Adjust padding-top as needed */}
+      <main style={{ flex: '1', padding: '0px 0 0 0' }}> {/* Adjust padding-top as needed */}
         {children}
       </main>
       <footer style={{ backgroundColor: '#C51D34', padding: '10px', color: 'white', textAlign: 'center' }}>
