8ee4acf636c474c28b4171ad47064acdbb7b44ad
Erick <erick.espinoza.710@my.csun.edu>
2025-04-09 17:39:25 -0700

    editor roles

--- frontend/src/Components/Kanban/KanbanBoard.jsx
+++ frontend/src/Components/Kanban/KanbanBoard.jsx
@@ -6,9 +6,13 @@ import Board from "./Components/Board";
 
 const KanbanBoard = () => {
   const [user_id, setUser_id] = useState("");
+  const [user_role, setUser_role] = useState("");
+  const { board_id } = useParams();
+  // Decode the token to get user ID
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+
     if (!token) {
       console.error("No token found. Please log in.");
       return;
@@ -22,9 +26,22 @@ const KanbanBoard = () => {
     } catch (error) {
       console.error("Error decoding token:", error);
     }
+    const fetchUserRole = async () => {
+      try {
+        const user = jwtDecode(token);
+
+        const response = await axios.get(
+          `/api/userBoards/role/${user.id}/${board_id}`
+        );
+        setUser_role(response.data);
+        console.log("User role:", response.data);
+      } catch (error) {
+        console.error("Error fetching user role:", error);
+      }
+    };
+    fetchUserRole();
   }, []);
 
-  const { board_id } = useParams();
-  return <Board board_id={board_id} user_id={user_id} />;
+  return <Board board_id={board_id} user_id={user_id} user_role={user_role} />;
 };
 export default KanbanBoard;
