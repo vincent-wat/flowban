6d9ef096ec4149b3f72ba51877a6590457e85d24
Andrew Boghosian <andrew.bogho@gmail.com>
2025-04-19 14:01:51 -0700

    google sign in fix
    google sign in wasnt encoding correct jwt token

--- backend/utils/jwtGenerator.js
+++ backend/utils/jwtGenerator.js
@@ -3,19 +3,11 @@ require('dotenv').config();
 
 
 
-<<<<<<< Updated upstream
-function jwtGenerator(user_id) {
-  const payload = { id: user_id };
-    const token = jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1h" });
-    
-    //console.log("Generated JWT:", token);
-=======
 function jwtGenerator(user) {
   const payload = {
     id: user.id, // Ensure the user ID is included
     organization_id: user.organization_id, // Include other fields if necessary
   };
->>>>>>> Stashed changes
 
   return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1h" });
 }
