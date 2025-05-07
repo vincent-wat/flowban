27c5cc60e0a098d839af8ddbc2dbf88ee7da095c
Emilio <emiliomontes10@gmail.com>
2025-05-07 15:11:47 -0700

    Add postinstall script to patch pdfjs-dist image assets

---
+++ frontend/scripts/patchPdfJsAssets.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const assets = ["altText_add.svg", "altText_clear.svg"];
+
+const fromDir = path.resolve(__dirname, "../public/images");
+const toDir = path.resolve(__dirname, "../node_modules/pdfjs-dist/legacy/web/images");
+
+
+if (!fs.existsSync(toDir)) {
+  fs.mkdirSync(toDir, { recursive: true });
+}
+
+assets.forEach((filename) => {
+  const src = path.join(fromDir, filename);
+  const dest = path.join(toDir, filename);
+
+  if (fs.existsSync(src)) {
+    fs.copyFileSync(src, dest);
+    console.log(` Copied ${filename} to pdfjs-dist`);
+  } else {
+    console.warn(` Missing source asset: ${filename}`);
+  }
+});
